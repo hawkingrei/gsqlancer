1fa2fcb4fb29f0f2b38e327aa87f2ccfa1277f9f
Weizhen Wang <wangweizhen@pingcap.com>
2023-04-28 17:09:10 +0800

    update
    Signed-off-by: Weizhen Wang <wangweizhen@pingcap.com>

--- pkg/model/meta.go
+++ pkg/model/meta.go
@@ -1,11 +1,26 @@
 package model
 
+import "golang.org/x/exp/maps"
+
 type SessionMeta struct {
-	tables map[string]Table
+	tables map[string]*Table
 }
 
 func NewSessionMeta() *SessionMeta {
 	return &SessionMeta{
 		tables: make(map[string]Table),
 	}
+}
+
+func (s *SessionMeta) Reset() {
+	maps.Clear(s.tables)
+}
+
+func (s *SessionMeta) CreateTable(name string, table *Table) {
+	s.tables[name] = table
+}
+
+func (s *SessionMeta) GetTable(name string) (*Table, bool) {
+	table, ok := s.tables[name]
+	return table, ok
 }
